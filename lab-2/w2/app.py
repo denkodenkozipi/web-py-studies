79a50fc32b8d1327f29ce6c30dd5865c847d3054
denikodem <denikodem@yandex.ru>
2026-02-01 01:57:15 +0900

    complete second task

---
+++ lab-2/w2/app.py
@@ -0,0 +1,18 @@
+from models import User
+from fastapi import FastAPI
+
+app = FastAPI()
+
+users = [
+    User(name="John Doe", id=1,),
+    User(name="Barbara Dey", id=2,),
+    User(name="Leni Bibi", id=3,),
+    User(name="Tzar Ivan", id=4,),
+]
+@app.get("/users")
+def read_root():
+    return {"users": users}
+
+@app.post("/user/")
+async def create_user(user: User):
+    return {"message": "successfully!", "user": user}
